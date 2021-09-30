2bd748cdfd32d1e2e4415683df947cca100e4f8f
edrish23 <edrishnathani@gmail.com>
2021-09-30 18:52:30 +0530

    initial commit

---
+++ simpleadd.py
@@ -0,0 +1,8 @@
+a=5
+b=4
+sum=a+b
+print(sum)
+num1 =input ("enter a first number:")
+num2 =input ("enter a second number:")
+addition =float(num1) + float(num2)
+print(addition)
---
+++ simpleinterest.py
@@ -0,0 +1,18 @@
+"""
+p=10000
+t=5
+r=5
+ans=(p*t*r)/100
+print(ans)
+"""
+def simple_interest(p,t,r):
+
+ print('The Principal is',p)
+ print('The time of period is',t)
+ print('The Rate of interest',r)
+
+ si=(p*t*r)/100
+ print("Simple Interest is ",si)
+ return si
+
+simple_interest(8,6,8)
